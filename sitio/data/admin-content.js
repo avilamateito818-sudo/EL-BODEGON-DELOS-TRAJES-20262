edff1c1cbfcd198d39b6d825eae9acf8c3d7df7a
avilamateito818-sudo <avilamateito818@gmail.com>
2026-08-31 11:28:34 -0500

    Admin update (31/8/2026, 11:28:32)

--- sitio/data/admin-content.js
+++ sitio/data/admin-content.js
@@ -39,7 +39,7 @@ window.ADMIN_CONTENT = {
     {
       "id": "amthg7t14h4dcd",
       "container": "#batas",
-      "html": "Te traemos los mejores vestidos y trajes para tus celebraciones \n* bautizos\n* primera comunión \n*vestidos para fiesta \n* matrimonios<button type=\"button\" class=\"admin-ui admin-delbtn\" title=\"Quitar este elemento\">✕</button>"
+      "html": "Te traemos los mejores vestidos y trajes para tus celebraciones \n* bautizos\n* primera comunión \n*vestidos para fiesta \n* matrimonios<"
     }
   ],
   "addTitles": [
