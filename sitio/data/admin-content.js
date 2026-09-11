29cce3a594406241dcd4bfebc433b0b90c45ab9c
avilamateito818-sudo <avilamateito818@gmail.com>
2026-09-11 16:36:34 -0500

    Admin update (11/9/2026, 16:36:31)

--- sitio/data/admin-content.js
+++ sitio/data/admin-content.js
@@ -52,7 +52,7 @@ window.ADMIN_CONTENT = {
     {
       "id": "amtxffestkxmep",
       "container": "#batas",
-      "html": "VESTIDOS ELEGANTES de prueba"
+      "html": "VESITDOS ELEGANTES"
     }
   ],
   "addPhotos": [
@@ -102,11 +102,14 @@ window.ADMIN_CONTENT = {
   },
   "photoSettings": {
     "enero": {
-      "width": 401,
-      "height": 340
+      "width": 361,
+      "height": 300
     }
   },
   "editorStyles": {
-    "#batas > h3:nth-child(3)": "color: rgb(255, 77, 77);"
+    "e1789159718338_th9t": "position: absolute; left: 0px; top: 366px;",
+    "e1789159823961_9wuj": "background-color: rgb(72, 72, 101);",
+    "e1789159817135_erg3": "color: rgb(255, 215, 0); font-size: 23px; text-shadow: rgba(0, 0, 0, 0.6) 0px 0px 14px;",
+    "e1789159822257_kxc8": "color: rgb(255, 215, 0); font-size: 15px;"
   }
 };
