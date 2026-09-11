ba5308a9db7367b15a1a41e4c7c684a21a87a577
avilamateito818-sudo <avilamateito818@gmail.com>
2026-09-11 15:02:54 -0500

    Admin update (11/9/2026, 15:02:52)

--- sitio/data/admin-content.js
+++ sitio/data/admin-content.js
@@ -62,7 +62,8 @@ window.ADMIN_CONTENT = {
   },
   "editorStyles": {
     "e1789156569885_aw7r": "",
-    "e1789156384526_1e60": "background-color: rgb(72, 149, 168); color: rgb(226, 24, 24); opacity: 1;",
+    "e1789156970475_0uog": "--d: 360ms;",
+    "e1789156384526_1e60": "background-color: rgb(72, 149, 168); color: rgb(226, 24, 24); opacity: 0.85;",
     "e1789156825312_n9bu": ""
   }
 };
