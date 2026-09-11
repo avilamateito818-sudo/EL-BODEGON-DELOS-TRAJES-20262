bac04c59651a493405acd938f81e9086e58cbdce
avilamateito818-sudo <avilamateito818@gmail.com>
2026-09-11 14:55:14 -0500

    Admin update (11/9/2026, 14:55:12)

--- sitio/data/admin-content.js
+++ sitio/data/admin-content.js
@@ -61,6 +61,6 @@ window.ADMIN_CONTENT = {
     }
   },
   "editorStyles": {
-    "e1789156384526_1e60": "background-color: rgb(85, 120, 124);"
+    "e1789156384526_1e60": "background-color: rgb(72, 149, 168);"
   }
 };
